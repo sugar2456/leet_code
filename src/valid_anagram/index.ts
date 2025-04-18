6f88fc0a85ec30f6940609d598303c128a77a6ba
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-18 23:52:36 +0900

    Valid Anagramの処理の流れにコメントする

--- src/valid_anagram/index.ts
+++ src/valid_anagram/index.ts
@@ -1,3 +1,17 @@
+/**
+ * アナグラム検査関数  
+ * 
+ * [処理の流れ]  
+ * 長さが一致しているか  
+ * すでに一致しているか  
+ * charの配列に変換する  
+ * ソートする  
+ * charの配列を合体して、比較する  
+ * ※charの配列をそのまま比較しようとすると参照のチェックで一致しない
+ * @param s 検索対象の文字列
+ * @param t 検索ワード
+ * @returns 含めるか否か
+ */
 export default function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) {
         return false;
