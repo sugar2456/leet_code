a2077c5e126fb8e65953a158977c3cd896ff3363
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-18 10:38:42 +0900

    Peak Index in a Mountain Arrayテストが漏れていた

--- src/peak_index_in_a_mountain_array/peak_index_in_a_moutain_array.test.ts
+++ src/peak_index_in_a_mountain_array/peak_index_in_a_moutain_array.test.ts
@@ -1,11 +1,28 @@
 import peakIndexInMountainArray from "."
 
 describe("Peak Index in a Mountain Array", () => {
-    it("ピーク探索関数　正常系", () => {
-        const input = [0,1,0]
+    it("ピーク探索関数　正常系1", () => {
+        const input = [0,1,0];
         const actual = peakIndexInMountainArray(input);
         const expected = 1;
 
         expect(actual).toBe(expected);
     });
+
+    it("ピーク探索関数　正常系2", () => {
+        const input = [0,2,1,0];
+        const actual = peakIndexInMountainArray(input);
+        const expected = 1;
+
+        expect(actual).toBe(expected);
+    });
+
+    it("ピーク探索関数　正常系3", () => {
+        const input = [0,10,5,2];
+        const actual = peakIndexInMountainArray(input);
+        const expected = 1;
+
+        expect(actual).toBe(expected);
+    });
+
 })
