9e292093b803a7cbaa7ec9671538e1bbeb692e5b
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-19 00:00:19 +0900

    Valid Palindromeの処理の流れをコメントする

--- src/valid_palindrome/index.ts
+++ src/valid_palindrome/index.ts
@@ -1,3 +1,13 @@
+/**
+ * 回文判定関数  
+ * 
+ * [処理の流れ]  
+ * 英数字以外の処理を取り除いと小文字に変換する  
+ * 文字列を反転する  
+ * 反転前と反転後を比較する  
+ * @param s 検索対象の文字列
+ * @returns 回文かどうか
+ */
 export default function isPalindrome(s: string): boolean {
     // 文字列を小文字に変換し、英数字以外の文字を除去
     const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
