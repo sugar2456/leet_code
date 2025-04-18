b75b57862408ed881cbdb3dab59e3430a94882fb
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-18 23:39:01 +0900

    moveZeroesの処理の流れをコメント

--- src/move_zeroes/index.ts
+++ src/move_zeroes/index.ts
@@ -1,3 +1,14 @@
+/**
+ * 数値配列ないに0が存在すると後ろに移動する関数
+ * 
+ * [処理の流れ]  
+ * 挿入する配列のインデックスを初期化  
+ * 配列をループする  
+ * 挿入が発生するとinsertPosをインクリメントする  
+ * 挿入が終了したポジション以降は全て0で終わるので0で埋める  
+ * 
+ * @param nums 数値配列
+ */
 export default function moveZeroes(nums: number[]): void {
     let insertPos = 0;
     for (let i = 0; i < nums.length; i++) {
