e750fd4e5c476f0b7439abd4ceab35f39f90e82c
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-18 13:07:56 +0900

    containsDuplicateのdocsに処理の流れを記載

--- src/contains_duplicate/index.ts
+++ src/contains_duplicate/index.ts
@@ -1,4 +1,19 @@
-export default function containsDuplicate(nums: number[]): boolean {
+/**
+ * 重複判定関数
+ * 
+ * [処理の流れ]  
+ * 数値配列の長さが0なら重複はありえないのでfalseを返却する  
+ * メモ用の変数を初期化  
+ * 入力の数値配列をループする  
+ * メモに現在の値が存在するかチェックする  
+ * 現在の値をメモに追加する  
+ * 何もメモに値がヒットしないなら重複は存在しないので、  
+ * falseを返却する
+ * 
+ * @param nums 数値配列
+ * @returns 重複を含むか否か
+ */
+export default function containsDuplicate(nums: number[]): boolean { 
     if (nums.length === 0) {
         return false;
     }
