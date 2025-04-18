706c5abe1028eea3bb6e1485755f28b8a5be12d5
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-18 18:32:35 +0900

    Maximum Depth of Binary Tree実装

--- src/maximum_depth_of_binary_tree/index.ts
+++ src/maximum_depth_of_binary_tree/index.ts
@@ -15,6 +15,20 @@ export class TreeNode {
 // 問題
 // Given the root of a binary tree, return its maximum depth.
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+/**
+ * 最も深いノードの長さを取得する
+ * 
+ * [処理の流れ]  
+ * カウンターを初期化
+ * ノードがnullの場合は0を返す
+ * 左側の探索木の深さを取得
+ * 右側の探索木の深さを取得
+ * 左側の探索木の深さと右側の探索木の深さを比較し、最大値を取得
+ * その最大値に1を加算して返す
+ * @param root ノード
+ * @returns 探索木の深さ
+ */
 export function maxDepth(root: TreeNode | null): number {
     let count = 1;
     if (root == null) {
